edbbfa46febb5f30d2c1da2b068e172c5d511e23
david.hua <david.dong.hua@gmail.com>
2025-11-16 22:16:24 +0800

    docs/conf.py remove intersphinx_mapping

--- docs/conf.py
+++ docs/conf.py
@@ -32,12 +32,12 @@ extensions = [
     "sphinx.ext.intersphinx",
 ]
 
-intersphinx_mapping = {
-    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
-    "python": ("https://docs.python.org/3/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
-}
-intersphinx_disabled_domains = ["std"]
+# intersphinx_mapping = {
+#     "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+#     "python": ("https://docs.python.org/3/", None),
+#     "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+# }
+# intersphinx_disabled_domains = ["std"]
 
 templates_path = ["_templates"]
 
